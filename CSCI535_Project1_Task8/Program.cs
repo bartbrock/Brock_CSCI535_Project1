435ccb4626a369deb89d4e24a7fe6056b2c8efd7
Bart Brock <bbrock@clarkdataservices.com>
2016-09-04 22:08:51 -0400

    Add threading to web query

--- CSCI535_Project1_Task8/Program.cs
+++ CSCI535_Project1_Task8/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Threading;
 
 namespace CSCI535_Project1_Task8
 {
@@ -13,22 +14,34 @@ namespace CSCI535_Project1_Task8
         static void Main(string[] args)
         {
             /********* TASK 8 *************/
+
+            //Console.WriteLine("Press any key to display weather conditon and temperature");
+            //Console.ReadKey(); Console.WriteLine();
+
+            ThreadPool.QueueUserWorkItem(WebQuery);
+            //Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+
+            //Console.WriteLine("Main thread exits.");
+            Console.ReadKey();
+        }
+
+        static void WebQuery(Object stateInfo)
+        {
             int webIndexBase = 0;
             int webIndexStart = 0;
             int webIndexOffset = 0;
             string tempString, condString;
-            string baseSearchString = "Currently";
+            string currentSearchString = "Currently";
             string condSearchString = "class=\"cond\">";
             string tempSearchString = "class=\"temp\">";
 
             WebClient myWebClient = new WebClient();
             myWebClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
+            // Index to Current Weather Conditions Charleston
             string webString = myWebClient.DownloadString("http://www.accuweather.com/en/us/charleston-sc/29401/weather-forecast/330678");
-            webIndexBase = webString.IndexOf(baseSearchString);
-
-            Console.WriteLine("Press any key to display weather conditon and temperature");
-            Console.ReadKey(); Console.WriteLine();
+            webIndexBase = webString.IndexOf(currentSearchString);
 
             // Local Weather Condition
             webIndexStart = webString.IndexOf(condSearchString, webIndexBase) + condSearchString.Length;
@@ -40,10 +53,7 @@ namespace CSCI535_Project1_Task8
             webIndexStart = webString.IndexOf(tempSearchString) + tempSearchString.Length;
             webIndexOffset = webString.IndexOf("<", webIndexStart) - webIndexStart;
             tempString = webString.Substring(webIndexStart, webIndexOffset);
-            Console.WriteLine("{0} Degrees", tempString);
-
-            Console.WriteLine("\n\n");
-            Console.ReadKey();
+            Console.WriteLine("{0} Degrees\n\n", tempString);
         }
     }
 }
